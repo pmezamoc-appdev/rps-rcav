c380a6f6c513466fba8229e22180161d1f248cd4
pmezamoc <pmezamoc@chicagobooth.edu>
2023-01-25 00:42:56 +0000

    Final version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
     #render({:html => "<h1>Hello, world!</h1>".html_safe})
     render({:template => "game_templates/user_rock.html.erb"})
   end
+
 
   def play_paper
  
@@ -24,6 +25,21 @@ end
 render({:template => "game_templates/user_paper.html.erb"})
 
   end
+
+  def play_scissors
+ 
+    @comp_move = ['rock', 'paper', 'scissors'].sample
+
+if @comp_move == "rock"
+  @outcome = "lost"
+elsif @comp_move == "paper"
+  @outcome = "won"
+elsif @comp_move == "scissors"
+  @outcome = "tied"
+end
+
+render({:template => "game_templates/user_scissors.html.erb"})
+end
 
   def homepage
     render({:template => "game_templates/rules.html.erb"})
